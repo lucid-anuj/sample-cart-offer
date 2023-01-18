2d7946e90411e4ee5c0c65be9b1b9670121bf2e2
anuj jain <anujai@lucidity.cloud>
2023-01-18 10:01:33 +0530

    with mockserver

--- src/test/java/com/springboot/StudentApplicationTests.java
+++ src/test/java/com/springboot/StudentApplicationTests.java
@@ -1,5 +1,6 @@
 package com.springboot;
 
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +12,7 @@ public class StudentApplicationTests {
 
 	@Test
 	public void contextLoads() {
+
 	}
 
 }
